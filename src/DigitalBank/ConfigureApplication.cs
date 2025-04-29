57e464c4862b8c83ac4d9f322c395a9e943ac802
Enrique Martinez <emartinezc@gmail.com>
2025-04-29 17:06:30 -0600

    Configure the application

---
+++ src/DigitalBank/ConfigureApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalBank;
+
+public static class ConfigureApplication
+{
+    public static void Configure(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
---
+++ src/DigitalBank/ConfigureServices.cs
@@ -0,0 +1,42 @@
+﻿using DigitalBank.Application.Common;
+using DigitalBank.Infrastructure.Exceptions;
+using DigitalBank.Infrastructure.Persistence;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalBank;
+
+public static class ConfigureServices
+{
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.AddSwagger();
+        builder.AddDatabase();
+
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+        builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
+    }
+
+    private static void AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.CustomSchemaIds(type => type.FullName?.Replace('+', '.'));
+            options.InferSecuritySchemes();
+        });
+    }
+
+    private static void AddDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<DigitalBankDbContext>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
