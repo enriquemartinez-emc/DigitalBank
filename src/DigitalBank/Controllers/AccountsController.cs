3f94f157323f563434881cbe4a16d073c0fb9707
Enrique Martinez <emartinezc@gmail.com>
2025-04-29 16:24:54 -0600

    Add controllers

---
+++ src/DigitalBank/Controllers/AccountsController.cs
@@ -0,0 +1,35 @@
+﻿using DigitalBank.Application.Features.Accounts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalBank.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public AccountsController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAccount(
+        CreateAccountCommand command,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : BadRequest(result.Error!.ToProblemDetails(StatusCodes.Status400BadRequest));
+    }
+
+    [HttpGet("{accountId}/balance")]
+    public async Task<IActionResult> GetBalance(
+        Guid accountId,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetAccountBalanceQuery(accountId), cancellationToken);
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : BadRequest(result.Error!.ToProblemDetails(StatusCodes.Status400BadRequest));
+    }
+}
---
+++ src/DigitalBank/Controllers/TransfersController.cs
@@ -0,0 +1,24 @@
+﻿using DigitalBank.Application.Features.Transfers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalBank.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransfersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public TransfersController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTransfer(
+        CreateTransferCommand command,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : BadRequest(result.Error!.ToProblemDetails(StatusCodes.Status400BadRequest));
+    }
+}
