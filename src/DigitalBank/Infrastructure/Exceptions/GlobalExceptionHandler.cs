50aba53eccccd625a332a2e76e59309fac287e99
Enrique Martinez <emartinezc@gmail.com>
2025-04-29 16:07:02 -0600

    Add global exception handler

---
+++ src/DigitalBank/Infrastructure/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using DigitalBank.Domain.Common;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace DigitalBank.Infrastructure.Exceptions;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var error = exception switch
+        {
+            InvalidOperationException => new Error("Operation.Invalid", exception.Message),
+            _ => new Error("Server.Error", "An unexpected error occurred")
+        };
+
+        var problemDetails = error.ToProblemDetails(StatusCodes.Status500InternalServerError);
+        httpContext.Response.StatusCode = problemDetails.Status!.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+        return true;
+    }
+}
