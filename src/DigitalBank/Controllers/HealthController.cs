cd06ebc7bc7043f68e89671a334af3a92767fef5
Enrique Martinez <emartinezc@gmail.com>
2025-04-30 08:36:32 -0600

    Add health controller

---
+++ src/DigitalBank/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalBank.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("API is healthy");
+}
