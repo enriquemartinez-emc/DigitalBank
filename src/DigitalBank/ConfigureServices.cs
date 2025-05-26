caa5553611588d6dcf3300b7ec6757e13cfc98b6
Enrique Martinez <emartinezc@gmail.com>
2025-05-25 18:24:09 -0600

    tweak swagger generation

--- src/DigitalBank/ConfigureServices.cs
+++ src/DigitalBank/ConfigureServices.cs
@@ -1,11 +1,13 @@
 ﻿using DigitalBank.Application.Common;
 using DigitalBank.Infrastructure.Exceptions;
 using DigitalBank.Infrastructure.Persistence;
+using DigitalBank.Infrastructure.Swagger;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace DigitalBank;
@@ -36,7 +38,42 @@ public static class ConfigureServices
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(options =>
         {
-            options.CustomSchemaIds(type => type.FullName?.Replace('+', '.'));
+             options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "DigitalBank API",
+                Version = "v1",
+                Description = "API for managing banking operations"
+            });
+
+            options.SchemaFilter<EnumSchemaFilter>();
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+            options.SchemaGeneratorOptions.SupportNonNullableReferenceTypes = false;
+            options.CustomSchemaIds(type => type.Name); 
             options.InferSecuritySchemes();
         });
     }
